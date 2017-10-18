d7b17f07bc3f62a7c4c18ee6cc39324741ad253d
Liang Xu <liangxu@nle-tech.com>
2017-10-18 18:25:54 +0800

    optimize rank ajax

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -3,7 +3,7 @@ import { List, ActivityIndicator } from 'antd-mobile';
 import Book from './Book';
 export default class RankComponent extends Component {
     componentDidMount() {
-        this.props.getRank()
+        this.props.rank.length || this.props.getRank()
     }
     render() {
         return (
